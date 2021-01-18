c9823557a2cef686c873d6c8402538750af36e8d
ANONIMYSCOALLA <58992611+ANONIMYSCOALLA@users.noreply.github.com>
2021-01-18 17:54:23 +0300

    Update 1.bat

--- 3/1.bat
+++ 3/1.bat
@@ -1,1 +1,6 @@
-sc query type=service | find "SERVICE_NAME"  > %1
+net start > c:\LAB6\punkt1.txt
+net stop Dnscache
+TIMEOUT /t 10
+net start > F:\3\punkt2.2.txt
+F:\3\nomer3.1.bat
+net start Dnscache
